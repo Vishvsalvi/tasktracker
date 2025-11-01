eeedbbd4c8f4d4bde9749ea9b0e277694e521cc9
Vishv Salvi <vishvsalvi@Vishvs-MacBook-Air.local>
2025-11-01 10:42:54 +0530

    prisma client

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,14 +1,9 @@
-import { PrismaClient } from "@/lib/generated/prisma/client";
+// lib/prisma.ts
+import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["error", "warn"] : ["error"],
-  });
+  globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
